3cdfd1d5dff9175f88c9571a293128e4a9527c51
Austin Jess <ajess33@gmail.com>
2018-05-24 10:28:13 -0500

    More EJS / Added Partials

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -1,13 +1,26 @@
 let express = require('express');
 let app = express();
 
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.render('home.ejs');
+  res.render('home');
 });
 
 app.get('/fallinlovewith/:thing', (req, res) => {
   let thing = req.params.thing;
-  res.render('love.ejs', { thingVar: thing });
+  res.render('love', { thingVar: thing });
+});
+
+app.get('/posts', (req, res) => {
+  let posts = [
+    { title: 'Post 1', author: 'Susy' },
+    { title: 'My day', author: 'Colt' },
+    { title: 'My night', author: 'Austin' }
+  ];
+
+  res.render('posts', { posts: posts });
 });
 
 app.listen(3000, () => {
