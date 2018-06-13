ee925448c460542b512d293c4e30fe46d04e1f74
Austin Jess <austinjess@Austins-MacBook-Pro-2.local>
2018-06-13 10:18:18 -0500

    Started Authentication

---
+++ Authentication/AuthDemo/app.js
@@ -0,0 +1,38 @@
+const express = require('express'),
+  mongoose = require('mongoose'),
+  passport = require('passport'),
+  bodyParser = require('body-parser'),
+  LocalStrategy = require('passport-local'),
+  passportLocalMongoose = require('passport-local'),
+  User = require('./models/user');
+
+mongoose.connect('mongod://localhost/auth_demo_app');
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use(
+  require('express-session')({
+    secret: 'Morgan is the best girlfriend in the world',
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// decoding and encoding
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/secret', (req, res) => {
+  res.render('secret');
+});
+
+app.listen(3000, () => {
+  console.log('Server started');
+});
---
+++ Authentication/readme.md
@@ -0,0 +1,11 @@
+#Authentication
+
+##Intro to Auth
+
+- What tools are we using?
+  - Passport
+  - Passport Local
+  - Passport Local Mongoose
+- Walk through auth flow
+- Discuss sessions
+  - Express-Session
