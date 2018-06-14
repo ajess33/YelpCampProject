9c399a90fe628c7ec7304cb1094545d42db67929
Austin Jess <austinjess@Austins-MacBook-Pro-2.local>
2018-06-14 18:21:32 -0500

    Sign Up Ability

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -6,11 +6,11 @@ const express = require('express'),
   passportLocalMongoose = require('passport-local'),
   User = require('./models/user');
 
-mongoose.connect('mongod://localhost/auth_demo_app');
+mongoose.connect('mongodb://localhost/auth_demo_app');
 
 const app = express();
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   require('express-session')({
     secret: 'Morgan is the best girlfriend in the world',
@@ -25,12 +25,39 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// ==================
+// ROUTES
+// ==================
+
 app.get('/', (req, res) => {
   res.render('home');
 });
 
 app.get('/secret', (req, res) => {
   res.render('secret');
+});
+
+// Auth Routes
+
+// show sign up form
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+// handling user sign up
+app.post('/register', (req, res) => {
+  User.register(
+    new User({ username: req.body.username }),
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        console.log(err);
+        return res.render('register');
+      }
+      passport.authenticate('local')(req, res, () => {
+        res.redirect('/secret');
+      });
+    }
+  );
 });
 
 app.listen(3000, () => {
