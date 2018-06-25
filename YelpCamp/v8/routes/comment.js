bb0f93bcf9082cc3ba3a70d58f18dc5d10398d46
Austin Jess <ajess33@gmail.com>
2018-06-25 16:06:52 -0500

    Comment Edit and Update

--- YelpCamp/v8/routes/comment.js
+++ YelpCamp/v8/routes/comment.js
@@ -41,6 +41,35 @@ router.post('/', isLoggedIn, (req, res) => {
   });
 });
 
+// COMMENT EDIT ROUTE
+router.get('/:comment_id/edit', (req, res) => {
+  Comment.findById(req.params.comment_id, (err, foundComment) => {
+    if (err) {
+      res.redirect('back');
+    } else {
+      res.render('comments/edit', {
+        campground_id: req.params.id,
+        comment: foundComment
+      });
+    }
+  });
+});
+
+// COMMENT UPDATE
+router.put('/:comment_id', (req, res) => {
+  Comment.findByIdAndUpdate(
+    req.params.comment_id,
+    req.body.comment,
+    (err, updatedComment) => {
+      if (err) {
+        res.redirect('back');
+      } else {
+        res.redirect('/campgrounds/' + req.params.id);
+      }
+    }
+  );
+});
+
 // Middleware
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
